a0bb9910778bdd3fe640b0e16c037b0ff9324f45
bradleyplater <bradley.plater1@gmail.com>
2025-07-17 16:03:27 +0100

    WW-30 - Added spotlight cards

---
+++ app/components/spotlight-card/spotlight-card.tsx
@@ -0,0 +1,7 @@
+export default function SpotlightCard() {
+  return (
+    <div className="border border-gray-700 rounded-lg p-4 w-full aspect-square flex items-center justify-center">
+      <span>This is a spotlight</span>
+    </div>
+  );
+}
--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,3 +1,4 @@
+import SpotlightCard from "~/components/spotlight-card/spotlight-card";
 import logoDark from "./logo-dark.svg";
 import logoLight from "./logo-light.svg";
 
@@ -6,28 +7,13 @@ export function Welcome() {
     <main className="flex items-center justify-center pt-16 pb-4">
       <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
         <header className="flex flex-col items-center gap-9">
+          Warriors logo here
         </header>
-        <div className="max-w-[300px] w-full space-y-6 px-4">
-          <nav className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4">
-            <p className="leading-6 text-gray-700 dark:text-gray-200 text-center">
-              What&apos;s next? This is deployed from github actionss
-            </p>
-            <ul>
-              {resources.map(({ href, text, icon }) => (
-                <li key={href}>
-                  <a
-                    className="group flex items-center gap-3 self-stretch p-3 leading-normal text-blue-700 hover:underline dark:text-blue-500"
-                    href={href}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    {icon}
-                    {text}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </nav>
+        <div className="px-4 w-full grid grid-cols-1 md:grid-cols-4 gap-4 auto-rows-fr">
+            <SpotlightCard/>
+            <SpotlightCard/>
+            <SpotlightCard/>
+            <SpotlightCard/>
         </div>
       </div>
     </main>
